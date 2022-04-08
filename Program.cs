c0702335c27da25e7f01e2931c3ab35673c75310
paulabrufato <paulabrufato.pb@gmail.com>
2022-04-08 15:58:59 -0300

    paulabrufato[dotnetAula8]

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aula8CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
